7391f81736572f8323e07bcaa1198b0e7f7a4624
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-01-19 18:18:01 +0100

    Change ids to refs and add scroll buttons

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,29 @@ const loadFromUrl = () => {
     const countWorkdays = params.get("wd") === "true";
     const title = params.get("title");
 
-    console.log({ until, countWorkdays, title });
-
     return { until, countWorkdays, title };
 };
 
 const updateUrl = (title, untilDate, countWorkdays) => {
-    const url = `/?title=${title}&until=${format(untilDate, "yyyy-MM-dd'T'hh:mm:ss")}&wd=${countWorkdays}`;
+    const url = `/?title=${title}&until=${format(
+        untilDate,
+        "yyyy-MM-dd'T'hh:mm:ss"
+    )}&wd=${countWorkdays}`;
     window.history.replaceState({}, document.title, url);
-}
+};
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         const data = loadFromUrl();
+
         this.handle = null;
+        this.mainEl = React.createRef();
+        this.headerEl = React.createRef();
+
         this.state = {
+            isConfigured: data.until !== null,
             untilDate: data.until ? new Date(data.until) : new Date(),
             countWorkdays: data.countWorkdays || false,
             title: data.title || "",
@@ -40,38 +46,54 @@ class App extends Component {
         this.onChangeUntilDate = this.onChangeUntilDate.bind(this);
         this.onChangeCountWorkdays = this.onChangeCountWorkdays.bind(this);
         this.updateTimer = this.updateTimer.bind(this);
+        this.onClickScrollTo = this.onClickScrollTo.bind(this);
+    }
+
+    onClickScrollTo(ref) {
+        return () => {
+            ref.current.scrollIntoView({ behavior: "smooth" });
+        };
     }
 
     onChangeTitle(evt) {
         const { untilDate, countWorkdays } = this.state;
         const title = evt.target.value;
 
-        this.setState({
-            title: title
-        }, () => updateUrl(title, untilDate, countWorkdays));
+        this.setState(
+            {
+                title: title
+            },
+            () => updateUrl(title, untilDate, countWorkdays)
+        );
     }
 
     onChangeUntilDate(evt) {
         const { title, countWorkdays } = this.state;
         const untilDate = new Date(evt.target.value);
 
-        this.setState({
-            untilDate: untilDate
-        }, () => {
-            updateUrl(title, untilDate, countWorkdays)
-            this.updateTimer();
-        });
+        this.setState(
+            {
+                untilDate: untilDate
+            },
+            () => {
+                updateUrl(title, untilDate, countWorkdays);
+                this.updateTimer();
+            }
+        );
     }
 
     onChangeCountWorkdays(evt) {
         const { title, untilDate } = this.state;
         const countWorkdays = evt.target.checked;
 
-        this.setState({
-            countWorkdays: countWorkdays
-        }, () => {
-            updateUrl(title, untilDate, countWorkdays);
-        });
+        this.setState(
+            {
+                countWorkdays: countWorkdays
+            },
+            () => {
+                updateUrl(title, untilDate, countWorkdays);
+            }
+        );
     }
 
     updateTimer() {
@@ -102,6 +124,9 @@ class App extends Component {
     }
 
     componentDidMount() {
+        if (!this.state.isConfigured) {
+            this.mainEl.current.scrollIntoView({ behavior: "smooth" });
+        }
         this.setState({
             timeLeft: calculateRemainingTime(this.state.untilDate)
         });
@@ -118,11 +143,27 @@ class App extends Component {
 
         return (
             <div className="App">
-                <header className="App-Header">
+                <header className="App-Header" ref={this.headerEl}>
                     <h1 className="App-Title">{title}</h1>
-                    {timeLeft != null && <Timer time={timeLeft} />}
+                    {timeLeft != null ? (
+                        <Timer time={timeLeft} />
+                    ) : (
+                        <div>↓ Enter config below ↓</div>
+                    )}
+                    <button
+                        onClick={this.onClickScrollTo(this.mainEl)}
+                        className="App-ScrollBtn App-ScrollBtn--Light"
+                    >
+                        ↓
+                    </button>
                 </header>
-                <main className="App-Main">
+                <main className="App-Main" ref={this.mainEl}>
+                    <button
+                        onClick={this.onClickScrollTo(this.headerEl)}
+                        className="App-ScrollBtn App-ScrollBtn--Dark"
+                    >
+                        ↑
+                    </button>
                     <h2 className="App-SectionTitle">Configuration</h2>
                     <div className="App-Config">
                         <label className="App-Label">
@@ -139,7 +180,10 @@ class App extends Component {
                             <input
                                 className="App-Input"
                                 type="datetime-local"
-                                value={format(untilDate, "yyyy-MM-dd'T'hh:mm:ss")}
+                                value={format(
+                                    untilDate,
+                                    "yyyy-MM-dd'T'hh:mm:ss"
+                                )}
                                 onChange={this.onChangeUntilDate}
                             />
                         </label>
@@ -151,7 +195,9 @@ class App extends Component {
                                 checked={countWorkdays}
                                 onChange={this.onChangeCountWorkdays}
                             />
-                            <span className="App-LabelText">Count Workdays</span>
+                            <span className="App-LabelText">
+                                Count Workdays
+                            </span>
                         </label>
                     </div>
                 </main>
