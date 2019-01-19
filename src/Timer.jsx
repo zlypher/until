c3f2d60718b87578d18efdaf574c5ffa067de6c1
Thomas Prochazka <thomas.prochazka@gmail.com>
2019-01-19 21:25:47 +0100

    Add more small tweaks

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -4,7 +4,11 @@ import "./Timer.css";
 const msToTime = msecs => {
     const ms = msecs % 1000;
     const secs = Math.floor(msecs / 1000);
-    let d = Math.floor(secs / (60 * 60 * 24));
+
+    let w = Math.floor(secs / (60 * 60 * 24 * 7));
+
+    let divDays = secs % (60 * 60 * 24 * 7);
+    let d = Math.floor(divDays / (60 * 60 * 24));
 
     let divHours = secs % (60 * 60 * 24);
     let h = Math.floor(divHours / (60 * 60));
@@ -15,27 +19,29 @@ const msToTime = msecs => {
     let divSeconds = divMinutes % 60;
     let s = Math.ceil(divSeconds);
 
-    return {
-        d: d,
-        h: `${h}`.padStart(2, "0"),
-        m: `${m}`.padStart(2, "0"),
-        s: `${s}`.padStart(2, "0"),
-        ms: `${ms}`.padStart(3, "0")
-    };
+    return { w, d, h, m, s, ms };
+};
+
+const simplePluralize = (count, word) => {
+    return count === 1 ? `${count} ${word}` : `${count} ${word}s`;
 };
 
 export const Timer = ({ time }) => {
-    const { d, h, m, s, ms } = msToTime(time);
+    const { w, d, h, m, s, ms } = msToTime(time);
+
+    const hStr = `${h}`.padStart(2, "0");
+    const mStr = `${m}`.padStart(2, "0");
+    const sStr = `${s}`.padStart(2, "0");
+    const msStr = `${ms}`.padStart(3, "0");
 
     return (
         <div className="Timer">
-            {d > 0 && (
-                <div>
-                    {d} days
-                </div>
-            )}
             <div>
-                {h}:{m}:{s}.{ms}
+                {w > 0 && `${simplePluralize(w, "week")} and `}
+                {d > 0 && simplePluralize(d, "day")}
+            </div>
+            <div>
+                {hStr}:{mStr}:{sStr}.{msStr}
             </div>
         </div>
     );
